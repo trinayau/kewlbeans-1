9e310d656e9d353dd623c42e37b360770fa597a0
Trina <92634994+trinayau@users.noreply.github.com>
2022-04-04 22:26:48 +0100

    add post container and changing app.js

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const fs = require("fs"); //importing file-system package
+const bodyParser = require("body-parser");
+const data = fs.readFileSync('./reviews.json');
+const reviews = JSON.parse(data);
+/*Use express middleware in request processing pipeline*/
+readJSON();
+app.use(bodyParser.text());
+app.use(express.json());
 app.use(cors());
 
 
