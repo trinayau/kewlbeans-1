03f73c545b594218d40e5215814db4a4aadc56eb
LibertySprackling <libertysprackling@googlemail.com>
2022-04-04 17:13:43 +0100

    setting up test suite and json file to store user input data

--- Server/app.js
+++ Server/app.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const fs = require('fs')
+
+try {
+    const jsonString = fs.readFileSync('data.json');
+    const entries = JSON.parse(jsonString);
+} catch (err) {
+    console.log(err);
+}
+
+
 app.use(cors());
 
+app.get('/', (req, res) => res.send('Hello World!'));
 
 
-module.exports = app
+
+module.exports = app;
